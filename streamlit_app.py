958d70d61f731d54031639c2b58864ea8e2eccb3
GrigoryMaslov <142306356+GrigoryMaslov@users.noreply.github.com>
2023-08-15 19:20:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,25 @@ import torch
 import torchvision as tv
 from torchvision import transforms as T
 
+from funcs import plot_bboxes
+
 url = 'https://github.com/GrigoryMaslov/BarkBeetle-RetinaNet/releases/download/v1.0.0/RetinaNetDD_40'
 filename = url.split('/')[-1]
 urllib.request.urlretrieve(url, filename)
+
+damage_detection_model = tv.models.detection.retinanet_resnet50_fpn_v2(
+                                  weights=None,
+                                  nms_thresh = 0.5,
+                                  topk_candidates = 200,
+                                  detections_per_img = 100,
+                                  score_thresh = 0.15,
+                                  num_classes=2)
+
+damage_detection_model.load_state_dict(torch.load('RetinaNetDD_40', map_location=torch.device('cpu') ))
+
+def main():
+  image = Image.open("sample_image.jpg")
+  st.image(image)
+  st.markdown("# All done!")
+
+main()
